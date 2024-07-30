e2347cb64ec3ae3eaeeb0921683504653aeadef1
Kukhyeon Nam <ska5686a@gmail.com>
2024-07-31 00:51:15 +0900

    0x01강의 연습문제2

---
+++ 0x01_원소찾기1.cpp
@@ -0,0 +1,15 @@
+﻿/*주어진 길이 N의 배열 arr에서 합이 100인 서로 다른 위치의 두 원소가 존재하면 1, 존재하지 않으면 0을 반환하는 함수 func2를 작성하라*/
+#include <iostream>
+using namespace std;
+
+void func2(int arr[], int N) {
+
+}
+
+int main() {
+	func2({ 1,52,48 }, 3);
+	func2({ 50,42 }, 2);
+	func2({ 4, 13, 63, 87 }, 4);
+
+}
+////

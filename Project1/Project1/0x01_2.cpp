57b3b6208eac018a0d7c92fad4af7e64da44a16b
Kukhyeon Nam <ska5686a@gmail.com>
2024-07-31 01:27:19 +0900

    GPT를 통해 내가 무슨 오류를 저질렀는지 확인했다. 1. main에서 배열을 미리 선언안함. 2. func2의 반환 타입.. 나는 void로 한번에 출력되기를 원했기 때문에 오류인지는 애매함.
    그래도 로직은 맞아서 뿌듯하다

--- Project1/Project1/0x01_2.cpp
+++ Project1/Project1/0x01_2.cpp
@@ -1,31 +1,31 @@
-﻿/*주어진 길이 N의 배열 arr에서 합이 100인 서로 다른 위치의 두 원소가 존재하면 1, 존재하지 않으면 0을 반환하는 함수 func2를 작성하라*/
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-void func2(int arr[], int N) {
-	int i = 0;
-	int j = N - 1;
-	int cnt = 0;
-	while (i < j) {
-		if (arr[i] + arr[j] > 100) {
-			j--;
-		}
-		else if (arr[i] + arr[j] < 100) {
-			i++;
-		}
-		else { // 두 원소의 합이 100이면
-			i++;
-			j++;
-			cnt++;
-		}
-	}
-	cout << cnt;
+int func2(int arr[], int N) {
+    int i = 0;
+    int j = N - 1;
+    while (i < j) {
+        if (arr[i] + arr[j] > 100) {
+            j--;
+        }
+        else if (arr[i] + arr[j] < 100) {
+            i++;
+        }
+        else { // 두 원소의 합이 100이면
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int main() {
-	func2({ 1,52,48 }, 3);
-	func2({ 50,42 }, 2);
-	func2({ 4, 13, 63, 87 }, 4);
+    int arr1[] = { 1, 52, 48 };
+    int arr2[] = { 50, 42 };
+    int arr3[] = { 4, 13, 63, 87 };
 
+    cout << func2(arr1, 3) << endl; // 결과: 1
+    cout << func2(arr2, 2) << endl; // 결과: 0
+    cout << func2(arr3, 4) << endl; // 결과: 0
+
+    return 0;
 }
-////

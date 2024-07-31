98e44b44e7236bed378d091011c28dca3a48a329
Kukhyeon Nam <ska5686a@gmail.com>
2024-07-31 14:59:42 +0900

    traverse 함수 추가

--- 0x04_Linked_List/0x04_Linked_List/간단한연결리스트.cpp
+++ 0x04_Linked_List/0x04_Linked_List/간단한연결리스트.cpp
@@ -11,6 +11,14 @@ public:
         fill(pre, pre + MX, -1);
         fill(nxt, nxt + MX, -1);
     }
+    void traverse() {
+        int cur = nxt[0];
+        while (cur != -1) {
+            cout << dat[cur] << ' ';
+            cur = nxt[cur];
+        }
+        cout << "\n\n";
+    }
 };
 
 const int Linked_list::MX;

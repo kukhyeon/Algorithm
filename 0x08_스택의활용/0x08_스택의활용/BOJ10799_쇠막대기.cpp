2f5c1d57db4b00958f5c1594106cffc8b71c1a66
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-04 01:16:59 +0900

    내가 작성한 코드를 보완해봤다. 작은 수의 괄호에는 작동하지만, 괄호가 많아지면 제대로 처리를 못하는 모습을 보였다. 이는 레벨이 낮아지는 상황을 프로그램이 선택하지 못해서 발생한다. 내가 작성한 코드는 과거의 상황에 귀속되어 막대의 수를 계산하는 거였으나, 문제의 풀다보니 이후 들어오는 괄호에 따라 조건에 맞게 동적으로 처리할 일이 계속 발생하는 걸 느꼈다.

--- 0x08_스택의활용/0x08_스택의활용/BOJ10799_쇠막대기.cpp
+++ 0x08_스택의활용/0x08_스택의활용/BOJ10799_쇠막대기.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <stack>
 using namespace std;
 
@@ -9,7 +10,8 @@ int main() {
 	int Dstick = 0;
 	int level = 0;
 
-	for (auto c : input) { // input
+	for (int i = 0; i < input.length(); i++) { // input
+		char c = input[i];
 		if (c == '(') {
 			if (!S.empty()) {
 				if (S.top() == '(') {
@@ -22,7 +24,13 @@ int main() {
 		else if (c == ')') { // input
 			if (!S.empty()) {
 				if (S.top() == '(') {
-					Dstick += level;
+					if (i > 2 && input[i - 2] == '(') { // laser
+						Dstick += level;
+						level--;
+					}
+					else {
+						Dstick += level+1;
+					}
 				}
 
 				else if (S.top() == ')') {
@@ -31,6 +39,7 @@ int main() {
 						level--;
 					}
 				}
+				S.push(c);
 			}
 		}
 	}

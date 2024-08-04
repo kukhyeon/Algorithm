2f0193e8d368fbd514096e1aa175508fb2a103be
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-05 00:53:01 +0900

    실버 난이도의 스택 문제. 예시 코드도 잘 나와 맞았다고 생각했는데, 검사하니까 틀렸다고 나왔다.

---
+++ 0x08_스택의활용/0x08_스택의활용/BOJ3986(S5)_좋은단어.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	int goodword = 0;
+	for (int i = 0; i < num; i++) {
+		stack<char> S;
+		string input;
+		cin >> input;
+		for (auto c : input) {
+			if (!S.empty()) {
+				if (S.top() == c) {
+					S.pop();
+				}
+			}
+			else {
+				S.push(c);
+			}
+		}
+		if (S.empty()) {
+			goodword++;
+		}
+	}
+	cout << goodword;
+}

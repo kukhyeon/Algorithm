fbf1795a68f8ce9b3fba3819fffb59a361a93518
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-05 21:29:07 +0900

    몇 시간 동안 끙끙 앓았는데.. 이유는 바로 board를 초기화 하는 과정에서 vis[0][0]에 값을 넣어서 그렇다. 내가 방점을 찍은 이유는 단순히 기존 BFS템플릿을 따라한 건데, 이거 하나 때문에 시간을 엄청 손해봤다.

--- 0x09_BFS/0x09_BFS/BOJ1926(S1)_그림.cpp
+++ 0x09_BFS/0x09_BFS/BOJ1926(S1)_그림.cpp
@@ -11,7 +11,7 @@ int main() {
 	cin >> cal;
 	int** board = new int* [raw];  // 힙 메모리로 이동, raw개의 int* 타입 포인터 배열 할당
 	for (int i = 0; i < raw; i++) { // 각 행에 대해 다시 메모리를 할당
-		board[i] = new int[cal]();
+		board[i] = new int[cal](); 
 	}
 	bool** vis = new bool* [raw];  // 힙 메모리로 이동, raw개의 int* 타입 포인터 배열 할당
 	for (int i = 0; i < raw; i++) { // 각 행에 대해 다시 메모리를 할당
@@ -20,14 +20,9 @@ int main() {
 	int dx[4] = { 1,0,-1,0 };
 	int dy[4] = { 0,1,0,-1 };
 
-	int paint;
 	for (int i = 0; i < raw; i++) {
 		for (int j = 0; j < cal; j++) {
-			cin >> paint;
-			board[i][j] = paint;
-			if (i == 0 && j == 0) {
-				vis[0][0] = paint;
-			}
+			cin >> board[i][j];
 		}
 	}
 
@@ -36,30 +31,31 @@ int main() {
 
 	int cnt = 0;
 	int maxnum = 0;
+
 	for (int i = 0; i < raw; i++) {
 		for (int j = 0; j < cal; j++) {
-			if (vis[i][j] || board[i][j] != 1) { // 수정 1: 조건 추가
-				continue;
-			}
-			Q.push({ i,j });
-			vis[i][j] = 1; // 수정 1: 방문 표시 추가
-			int temp = 1; // 수정 1: temp 초기화 위치 변경
-
-			if (board[i][j] == 1) {
+			if (board[i][j] == 1 && !vis[i][j]) {
+				cnt++;
 				Q.push({ i, j });
+				vis[i][j] = 1;
+				int temp = 0;
+\
 				while (!Q.empty()) {
 					pair<int, int> cur = Q.front(); Q.pop();
+					temp++;
 					for (int dir = 0; dir < 4; dir++) {
 						int nx = cur.X + dx[dir];
 						int ny = cur.Y + dy[dir];
-						if (nx < 0 || nx >= raw || ny < 0 || ny >= cal)continue;
-						if (vis[nx][ny] == 1 || board[nx][ny] != 1) continue;
+						if (nx < 0 || nx >= raw || ny < 0 || ny >= cal) {
+							continue;
+						}
+						if (vis[nx][ny] == 1 || board[nx][ny] != 1) {
+							continue;
+						}
 						vis[nx][ny] = 1;
-						temp++;
 						Q.push({ nx, ny });
 					}
 				}
-				cnt++;
 				if (temp > maxnum) {
 					maxnum = temp;
 				}

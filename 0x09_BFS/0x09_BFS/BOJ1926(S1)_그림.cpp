922e5cde689821ef60272fce8d419216185ccf24
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-05 20:22:07 +0900

    <수정사항> 1. 주석 추가 2. 최대 그림의 크기를 계산하는 메소드 추가. ---------------------------------------------- 내가 작성한 최종 코드.. 예시 입력은 잘 나오나 백준에 제출하니 34%에서 틀렸다고 나온다.

--- 0x09_BFS/0x09_BFS/BOJ1926(S1)_그림.cpp
+++ 0x09_BFS/0x09_BFS/BOJ1926(S1)_그림.cpp
@@ -9,12 +9,12 @@ int main() {
 	int raw, cal;
 	cin >> raw;
 	cin >> cal;
-	int** board = new int* [raw];  // 힙 메모리로 이동
-	for (int i = 0; i < raw; i++) {
-		board[i] = new int[cal]();
+	int** board = new int* [raw];  // 힙 메모리로 이동, raw개의 int* 타입 포인터 배열 할당
+	for (int i = 0; i < raw; i++) { // 각 행에 대해 다시 메모리를 할당
+		board[i] = new int[cal](); 
 	}
-	bool** vis = new bool* [raw];  // 힙 메모리로 이동
-	for (int i = 0; i < raw; i++) {
+	bool** vis = new bool* [raw];  // 힙 메모리로 이동, raw개의 int* 타입 포인터 배열 할당
+	for (int i = 0; i < raw; i++) { // 각 행에 대해 다시 메모리를 할당
 		vis[i] = new bool[cal]();
 	}
 	int dx[4] = { 1,0,-1,0 };
@@ -39,6 +39,8 @@ int main() {
 
 	//test
 	int cnt = 0;
+	int temp = 0;
+	int maxnum = 0;
 	for (int i = 0; i < raw; i++) {
 		for (int j = 0; j < cal; j++) {
 			if (vis[i][j]) {
@@ -54,16 +56,21 @@ int main() {
 						if (nx < 0 || nx >= raw || ny < 0 || ny >= cal)continue;
 						if (vis[nx][ny] == 1 || board[nx][ny] != 1) continue;
 						vis[nx][ny] = 1;
+						temp++;
 						Q.push({ nx, ny });
 					}
 				}
 				if (Q.empty()) {
 					cnt++;
+					if (temp > maxnum) {
+						maxnum = temp;
+					}
+					temp = 0;
 				}
 			}
 		}
 	}
-	cout << cnt;
+	cout << cnt<< '\n' << maxnum;
 	// 동적 할당된 메모리 해제
 	for (int i = 0; i < raw; i++) {
 		delete[] board[i];

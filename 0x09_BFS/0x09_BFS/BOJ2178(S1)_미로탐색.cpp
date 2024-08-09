61b94e122d68708c2bb98992f05a6e554e693078
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-10 00:46:34 +0900

    새롭게 작성한 코드. 보다시피 로직을 아예 갈아 엎었다. 생각해보니 굳이 역추적으로 코드를 작성할 필요 없이 각 좌표와 시작점 사이의 거리를 저장하는 2차원 배열을 하나 만들면 그게 최소 거리가 되는 것이다. 최소 거리라는 말에 혹해서 직접 거리 계산기를 들고 가는 상상을 해버렸다. 여러 아이디어를 염두에 두고 설계하는 것이 실력 발전에 도움이 될 것이다.

--- 0x09_BFS/0x09_BFS/BOJ2178(S1)_미로탐색.cpp
+++ 0x09_BFS/0x09_BFS/BOJ2178(S1)_미로탐색.cpp
@@ -12,30 +12,29 @@ int n, m;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 
-int BFS(queue<pair<int, int>> Q, int bread) {
-	while (!Q.empty()) { // Q가 비어있지 않을 때 까지
-		pair<int, int> cur = Q.front();
-		Q.pop();
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.X + dx[dir];
-			int ny = cur.Y + dy[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (vis[nx][ny] || board[nx][ny] != 1) continue;
-			vis[nx][ny] = 1;
-			bread++;
-			Q.push({ nx, ny });
-			if (Q.size() > 1) {
-				int min = 102;
-				for (int i = 0; i< Q.size(); i++) {
-					int temp = BFS(Q, 0);
-					if (temp > 0 && min > temp) {
-						min = temp;
-					}
-				}
-				return bread + min;
-			}
-		}
-	}
+int BFS(int sX, int sY) {
+    queue<pair<int, int>> Q;
+    int distance[102][102] = {}; // 각 좌표에 시작점과의 거리를 저장하는 2차원 배열 초기화
+    Q.push({ sX, sY });
+    vis[sX][sY] = true;
+    distance[sX][sY] = 1; // 시작점의 거리
+
+    while (!Q.empty()) {
+        pair<int, int> cur = Q.front();
+        Q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (vis[nx][ny] || board[nx][ny] != 1) continue;
+
+            vis[nx][ny] = true;
+            distance[nx][ny] = distance[cur.X][cur.Y] + 1; // board에 좌표에 대응하는, 시작점과의 최소 거리를 저장
+            Q.push({ nx, ny });
+        }
+    }
+    return distance[n - 1][m - 1]; // 도착점의 거리 반환
 }
 
 int main() {
@@ -54,6 +53,5 @@ int main() {
 			board[i][j] = inputs[i][j] - '0'; // 문자를 숫자로 변환
 		}
 	}
-	Q.push({ 0, 0 });
-	cout << BFS(Q, 1);
+	cout << BFS(0, 0);
 }

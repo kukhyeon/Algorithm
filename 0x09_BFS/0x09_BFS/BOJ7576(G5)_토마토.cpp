622072f5a1984985fd0ef13fce14b0c298ebc222
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-11 23:22:45 +0900

    내가 작성한 최종 코드. 모든 테스트 케이스와 반례는 잘 작동하지만, 온라인 저지에 제출하니 시간 초과 문제가 발생하였다.

--- 0x09_BFS/0x09_BFS/BOJ7576(G5)_토마토.cpp
+++ 0x09_BFS/0x09_BFS/BOJ7576(G5)_토마토.cpp
@@ -1,68 +1,78 @@
 ﻿#include<iostream>
 #include<queue>
+#include<algorithm>
 using namespace std;
+
 #define X first
 #define Y second
 
-/*
-	N은 상자의 세로 칸의 수, M은 상자의 가로 칸의 수 (2 <= M, N <= 1000)
-	정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않다.
-	하루가 지나면 이미 익어있는 토마토들의 인접한 토마토들은 익는다. 
-	모든 토마토들이 며칠이 지나면 다 익는지 최소 일수를 출력한다.
-*/
-
- /*
- 익은 토마토의 좌표 개수에 대하여 반복하며, 거리 좌표를 더 작은 숫자로 업데이트 한다.
- 거리 2차원 배열에서 가장 높은 값을 가지는 좌표를 찾아서 거리 값 출력 < 최소 날짜가 되겠다
- */
-
 int board[1002][1002];
-int n, m; 
+int dist[1002][1002]; // 각 좌표의 최소 거리를 저장할 배열
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
+bool vis[1002][1002]; // 방문 배열 초기화
 
 int main() {
-	int d[1002][1002] = {}; // 각 좌표에 시작점과의 거리를 저장하는 2차원 배열 초기화
-	cin.tie(0);
-	cin >> m >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			int num;
-			cin >> num;
-			board[i][j] = num;
-		}
-	}
+    int n, m;
+    cin >> m >> n;
 
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (board[i][j] == 1) {
-				queue<pair<int, int>> Q;
-				Q.push({ i, j });
-				bool vis[1002][1002] = {};
-				vis[i][j] = true;
-				d[i][j] = 1; // 시작점의 거리
-				while (!Q.empty()) {
-					pair<int, int> cur = Q.front();
-					int d_t[1002][1002] = {};
-					Q.pop();
-					for (int dir = 0; dir < 4; dir++) {
-						int nx = cur.X + dx[dir];
-						int ny = cur.Y + dy[dir];
-
-						if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-						if (vis[nx][ny] || board[nx][ny] != 0) continue;
-
-						vis[nx][ny] = true;
-						d_t[nx][ny] = d_t[cur.X][cur.Y] + 1; // board에 좌표에 대응하는, 시작점과의 최소 거리를 저장
-						Q.push({ nx, ny });
-						if (d_t[nx][ny] > 0 && d[nx][ny] > d_t[nx][ny]) { // 본 거리 좌표에 최소 거리를 새롭게 업데이트
-							d[nx][ny] = d_t[nx][ny];
-						}
-					}
-				}
-			}
-		}
-	}
-	cout << d[n - 1][m - 1] - 1;
+    // 거리 배열 초기화
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+            dist[i][j] = -1;
+        }
+    }
+    queue<pair<int, int>> Q;
+    // 각 익은 토마토에서 BFS 실행
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            /*
+                만약 익은 토마토가 board에 존재한다면, 익은 좌표에서 대하여 BFS를 실행한다.
+                현재의 거리 값과 이전의 거리 값을 비교하는 조건문을 통해 dist 배열을 업데이트 한다. 
+            */
+            if (board[i][j] == 1) {
+                Q.push({ i, j }); 
+                vis[i][j] = true;
+                dist[i][j] = 0;
+                while (!Q.empty()) {
+                    pair<int, int> cur = Q.front(); Q.pop();
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = cur.X + dx[dir];
+                        int ny = cur.Y + dy[dir];
+                        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                        // 이미 방문했거나 0이 아닌 값은 건너뛴다. 
+                        if (vis[nx][ny] || board[nx][ny] != 0) continue;
+                        vis[nx][ny] = true;
+                        if (dist[nx][ny] == -1) {
+                            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+                        }
+                        if (dist[nx][ny] > dist[cur.X][cur.Y] + 1) {
+                            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+                        }
+                        Q.push({ nx, ny });
+                    }
+                }
+                //vis 초기화
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        vis[i][j] = false;
+                    }
+                }
+            }
+        }
+    }
+    int maxnum = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (dist[i][j] == -1 && board[i][j] == 0) { // 익지 않은 토마토가 있는 경우
+                cout << -1; // 모든 토마토가 익지 못한 경우
+                return 0;
+            }
+            if (maxnum < dist[i][j]) {
+                maxnum = dist[i][j];
+            }
+        }
+    }
+    cout << maxnum;
 }

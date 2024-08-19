3016bcea7428ce40870c8c7d83e9f6655d4501a7
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-19 22:16:30 +0900

    문법 오류 수정 외엔 아무 도움도 안 받고 직접 풀었다. 사실 이 문제가 BFS라고 유형에 적혀있지 않았으면, 푸는데에 더 오랜 시간이 걸렸을 것이다. 알고리즘은 다음과 같다.
    0. BFS는 1차원으로 설계한다.
    1. 현재 위치 N에서 BFS를 시작한다.
    2. Queue에 {N-1, N+1, 2*N}을 push, 만약 이미 그 자리에 값이 들어있으면, 건너뛴다.
    3. Queue가 빌 때까지 반복한다.
    
    BFS가 많이 익숙해졌으나, 내가 주어진 상황이 알맞는(BFS를 사용함에) 상황인지 판단하는 능력이 필요할 것 같다.

---
+++ 0x09_BFS/0x09_BFS/BOJ1697(S1)_숨바꼭질.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+const int num = 100001;
+int line[num]; // 방점으로 사용
+
+int dx[3] = { -1, 1, 2 };
+
+int main() {
+	cin.tie(0);
+	queue<int> Q;
+	int N, K;
+	cin >> N >> K;
+
+	memset(line, -1, sizeof(line));
+	line[N] = 0;
+	Q.push(N);
+
+	while (!Q.empty()) {
+		int preQ = Q.front();
+		Q.pop();
+		for (int dir = 0; dir < 3; dir++) {
+			int nx;
+			if (dir == 2) {
+				nx = preQ * dx[dir];
+			}
+			else {
+				nx = preQ + dx[dir];
+			}
+			if (nx < 0 || nx >= num) continue;
+			if (line[nx] != -1) continue;
+			line[nx] = line[preQ] + 1;
+			Q.push(nx);
+		}
+	}
+	cout << line[K];
+}

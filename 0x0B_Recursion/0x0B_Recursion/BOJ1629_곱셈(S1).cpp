38a9e1fbd66c6e3fb29bdfe3b78f43685eb39910
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-20 22:29:31 +0900

    모듈러 거듭제곱 곱셈 연산을 재귀 방식으로 구현했다. 이전 순환 로직으로는 int 자료형을 초과하는 거듭제곱 연산이 불가하다.

--- 0x0B_Recursion/0x0B_Recursion/BOJ1629_곱셈(S1).cpp
+++ 0x0B_Recursion/0x0B_Recursion/BOJ1629_곱셈(S1).cpp
@@ -1,25 +1,35 @@
-#include <iostream>
-#include <vector>
+﻿#include <iostream>
 using namespace std;
+/*
+	모듈러 곱셈 연산
+	(a mod n * b mod n) mod n = (a * b) mod n에서 a = b이면,
+	(a mod n * a mod n) mod n = (a * a) mod n = a^2 mod n
+*/
+unsigned long long mod_exp(unsigned long long base, unsigned long long exp, unsigned long long mod) {
+	int result = 1;
+	// 재귀 종료 조건
+	if (exp == 0) {
+		return 1;
+	}
+	// 재귀 종료 조건2
+	if (exp == 1) {
+		return base % mod;
+	}
+	// 이진수 exp의 자리에 1이 존재하는지 확인
+	if (exp % 2 == 1) {
+		result = (result * base) % mod;
+	}
+	// 모듈러 곱셈 법칙에 의해 A^2 % C = {(A % C) * (A % C)} % C 이니까,
+	// 다음의 자릿수 후보를 업데이트 해줌
+	base = (base * base) % mod;
+	// 재귀 반환
+	result = (result * mod_exp(base, exp / 2, mod)) % mod;
 
-vector<int> mods;
+	return result;
+}
 
 int main() {
-	unsigned long long bottom, power, mod;
-	cin >> bottom >> power >> mod;
-	int result;
-	for (int i = 0; i < power; i++) {
-		bottom *= bottom;
-		mods.push_back(bottom % mod);
-		if (i > 0 && mods[0] == mods[i]) {
-			result = power % mods.size();
-			break;
-		}
-	}
-	if (mods.size() == 1) {
-		cout << mods[0];
-	}
-	else {
-		cout << mods[result];
-	}
+	unsigned long long base, exp, mod;
+	cin >> base >> exp >> mod;
+	cout << mod_exp(base, exp, mod);
 }

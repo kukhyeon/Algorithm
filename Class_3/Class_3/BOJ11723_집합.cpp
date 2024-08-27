631cb44d5bc99b9e5e0c6c3156534e5d18ba15cd
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-28 03:21:09 +0900

    중복되지 않은 set으로 작성한 코드. 시간 초과가 발생한다. 아마 find가 선형 탐색이라 그러지 않을까 싶다. 비트 마스킹으로 풀어야 한다는데, 내일 공부해와서 해야할 것 같다.

---
+++ Class_3/Class_3/BOJ11723_집합.cpp
@@ -0,0 +1,54 @@
+﻿//S5
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    int M;
+    cin >> M;
+    unordered_set<int> S; 
+
+    for (int i = 0; i < M; i++) {
+        string input;
+        int x;
+        cin >> input;
+
+        if (input == "add") {
+            cin >> x;
+            S.insert(x); // 중복되지 않도록 자동 처리
+        }
+        else if (input == "remove") {
+            cin >> x;
+            S.erase(x); // 존재하지 않으면 아무 일도 일어나지 않음
+        }
+        else if (input == "check") {
+            cin >> x;
+            if (S.find(x) != S.end()) {
+                cout << '1' << '\n';
+            }
+            else {
+                cout << '0' << '\n';
+            }
+        }
+        else if (input == "toggle") {
+            cin >> x;
+            if (S.find(x) != S.end()) {
+                S.erase(x); // 존재하면 제거
+            }
+            else {
+                S.insert(x); // 존재하지 않으면 추가
+            }
+        }
+        else if (input == "all") {
+            S.clear();
+            for (int j = 1; j <= 20; j++) { // 1부터 20까지 추가
+                S.insert(j);
+            }
+        }
+        else if (input == "empty") {
+            S.clear(); // 모든 요소 제거
+        }
+    }
+
+    return 0;
+}

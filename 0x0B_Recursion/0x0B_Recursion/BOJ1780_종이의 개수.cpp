37c19593b123624a31af7e777097fe3e20beea4f
Kukhyeon Nam <ska5686a@gmail.com>
2024-08-30 17:35:59 +0900

    변수명을 변경하고, 반복문 범위를 올바르게 수정했더니 잘 동작한다.

--- 0x0B_Recursion/0x0B_Recursion/BOJ1780_종이의 개수.cpp
+++ 0x0B_Recursion/0x0B_Recursion/BOJ1780_종이의 개수.cpp
@@ -1,36 +1,46 @@
-// S2
+﻿// S2
 #include <iostream>
 using namespace std;
 
-int arr[2189][2189];
+int arr[2189][2189] = {};
 int result1 = 0, result2 = 0, result3 = 0;
 
-void paper(int N, int BC1, int BC2) {
+void paper(int N, int row, int cal) {
 	bool checker = true;
-	int firstValue = arr[BC1][BC2];
-	for (int i = BC1; i < BC1 + N; i++) {
-		for (int j = BC2; j < BC2 + N; j++) {
+	int firstValue = arr[row][cal];
+	for (int i = row; i < row + (N); i++) {
+		for (int j = cal; j < cal + (N); j++) {
 			if (arr[i][j] != firstValue) {
+				//Test
+				//cout << i << ' ' << j << " firstValue: " << firstValue << "\n";
+				//cout << "arr[i][j]: " << arr[i][j] << "\n";
 				checker = false;
 				break;
 			}
 		}
+		if (!checker) break;
 	}
 	if (checker) {
 		if (firstValue == -1) {
+			//Test
+			//cout << row << " " << cal << " N: " << N << "\n";
 			result1++;
 		}
 		else if (firstValue == 0) {
+			//Test
+			//cout << row << " " << cal << " N: " << N << "\n";
 			result2++;
 		}
 		else if (firstValue == 1) {
+			//Test
+			//cout << row << " " << cal << " N: " << N << "\n";
 			result3++;
 		}
 	}
 	else {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				paper(N / 3, BC1 + i * N / 3, BC2 + j * N / 3);
+				paper(N / 3, row + i * N / 3, cal + j * N / 3);
 			}
 		}
 	}
@@ -41,8 +51,8 @@ int main() {
 	ios::sync_with_stdio(0);
 	int N;
 	cin >> N;
-	for (int i = 0; i < N - 1; i++) {
-		for (int j = 0; j < N - 1; j++) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			int num;
 			cin >> num;
 			arr[i][j] = num;

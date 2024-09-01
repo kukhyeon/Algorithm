de052f682496696225c04b2d91e09feed141c9c9
Kukhyeon Nam <ska5686a@gmail.com>
2024-09-01 16:11:09 +0900

    내가 작성한 최종 코드. 모든 TC는 통과하나, 시간 초과가 발생한다.

---
+++ 0x0B_Recursion/0x0B_Recursion/BOJ1042_Z.cpp
@@ -0,0 +1,48 @@
+﻿// S1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int cnt = 0;
+void recursion(int N, int r, int c ,int row, int col) {
+	bool checker = true;
+	int power = pow(2, N); // 2^N
+	//Test
+	//cout << "재귀 test row: " << row << " col " << col << endl;
+	if (N == 1) { // 2x2면
+		for (int i = row; i < row + 2; i++) {
+
+			for (int j = col; j < col + 2; j++) {
+				/*
+				cout << "행: " << i << endl;
+				cout << "열: " << j << endl;
+				cout << cnt << endl;
+				*/
+
+				if (i == r && j == c) {
+					cout << cnt;
+					checker = false;
+					break;
+				}
+				cnt++;
+			}
+			if (!checker) {
+				break;
+			}
+		}
+	}
+
+	else {
+		for (int i = 0; i < 2; i++){
+			for (int j = 0; j < 2; j++) {
+				recursion(N-1, r, c, row + i * (power / 2), col + j * (power / 2));
+			}
+		}
+	}
+};
+
+int main() {
+	int N, r, c;
+	cin >> N >> r >> c;
+	recursion(N, r, c, 0, 0);
+}

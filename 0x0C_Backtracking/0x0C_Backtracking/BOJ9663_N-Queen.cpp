b96f5f878305c48949fd46a432048d03d80cd4f3
Kukhyeon Nam <ska5686a@gmail.com>
2024-09-04 20:26:51 +0900

    백트래킹에서 아주 유명한 문제. 만약 배열 3개를 사용하는 로직을 몰랐으면, 한 재귀 호출당 2번의 반복문을 돌렸을지도..

---
+++ 0x0C_Backtracking/0x0C_Backtracking/BOJ9663_N-Queen.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int visited[20];
+int visited_right[40];
+int visited_left[40];
+int cnt = 0;
+
+void recursion(int N, int row) {
+	if (row == N) {
+		cnt++;
+		return;
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			if (visited[i] || visited_right[row-i+N-1] || visited_left[row+i]) {
+				continue;
+			}
+			visited[i] = 1;
+			visited_right[row-i+N-1] = 1;
+			visited_left[row+i] = 1;
+			// 다음 행으로 
+			recursion(N, row + 1);
+			// visit 해제
+			visited[i] = 0;
+			visited_right[row - i + N - 1] = 0;
+			visited_left[row + i] = 0;
+
+		}
+	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+	recursion(N, 0);
+	cout << cnt;
+}

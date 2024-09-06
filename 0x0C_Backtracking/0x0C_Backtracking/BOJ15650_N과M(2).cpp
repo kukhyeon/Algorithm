397562a0e412a65fc2c86731a60be0cc52f7c901
Kukhyeon Nam <ska5686a@gmail.com>
2024-09-06 21:15:14 +0900

    이러한 유형의 트리 구조는 visited를 굳이 쓸 이유가 없었다.

---
+++ 0x0C_Backtracking/0x0C_Backtracking/BOJ15650_N과M(2).cpp
@@ -0,0 +1,34 @@
+﻿// S3
+#include <iostream>
+using namespace std;
+
+int N, M;
+int num[9]; // index = 0 to 8
+int visited[9]; // index = 0 to 8
+
+// 상태 이상 트리, cnt는 0부터 M까지 성장, bc는 1부터 N까지 성장
+void ctree(int cnt, int bc) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << num[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = bc; i <= N; i++) {
+		if (!visited[i]) {
+			num[cnt] = i;        // 현재 선택한 숫자를 배열에 저장
+			ctree(cnt + 1, i + 1); // 다음 숫자는 현재 숫자 이후부터 선택
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	// index = 0 to 8
+	for (int i = 0; i < 9; i++) {
+		visited[i] = false;
+	}
+	ctree(0, 1);
+}

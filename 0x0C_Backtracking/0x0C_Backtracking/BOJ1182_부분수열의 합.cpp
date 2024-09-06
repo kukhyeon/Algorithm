b5209a21fd335d3134c0e0b3d996c09205fc1cb8
Kukhyeon Nam <ska5686a@gmail.com>
2024-09-06 20:17:25 +0900

    지금 보니 S를 전역 변수로 선언해서 굳이 매개변수로 넣지 않아도 됐던 것 같다.

---
+++ 0x0C_Backtracking/0x0C_Backtracking/BOJ1182_부분수열의 합.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int N, S;
+int arr[22];
+int visited[22];
+int cnt = 0;
+
+// sum은 값을 저장하는 변수, idx는 배열의 idx를 맵핑
+void recursion(int S, int sum, int idx) {
+	if (idx == N) {
+		if(sum == S) cnt++;
+		return;
+	}
+	recursion(S, sum, idx + 1);
+	recursion(S, sum + arr[idx], idx + 1);
+}
+
+int main() {
+	cin >> N >> S;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	recursion(S, 0, 0);
+	if (S == 0) cnt--;
+	cout << cnt;
+}
